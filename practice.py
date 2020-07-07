ef77c871c6d36fb2d9100aeb06cc63097cbe16f5
Surendra Bisht <surendra.bisht023@gmail.com>
2020-07-07 19:35:58 +0530

    added practice

---
+++ practice.py
@@ -0,0 +1,40 @@
+a=list([3,4,5])
+b=a * 2
+print(b)
+
+str1= 'sonu'
+
+for i in range(len(str1)-2):
+    str1[i].upper()
+
+b=([[5,6],[6,7]])
+print(str1)
+   
+print(b)
+
+n=[5,]
+n1=[5]
+print(n)
+print(n==n1)
+print(n is n1)
+print(type(n)==type(n1))   
+print(n*2)
+
+listing = [10,1,2,3,4,5,6,7,8,9]
+li1= list(filter((lambda e: e%3==0),listing))
+li2= list(filter((lambda e: e%2==0),listing))
+print(li1)
+print(li2)
+li2 =list( map((lambda l : l*2),li2))
+print(li2)
+
+li3=list( [17,16,20])
+li2.extend(li3)
+print(li2)
+li5=li2.append(50)
+print(li2)
+print(li5)
+li6=[23,45]
+print(li6)
+print("a" "b")
+
