d9494678239a74b1f9b50a7a89e40854ff25de03
Surendra Bisht <surendra.bisht023@gmail.com>
2020-07-07 10:01:09 +0530

    Day 7 changes

---
+++ Day1/Operators.py
@@ -0,0 +1,28 @@
+print(" remainder of 9/2 is ",9%2)
+print(" 9/2 is ",9/2)
+print(" floor division of 9 by 2 is ",9//2)   # used to get integer value of quotient.
+
+#integer
+a=5
+b=6
+#String
+c='5'
+d='6'
+#float
+e=4.5 
+f=5.3
+
+#addition
+print(a+b)
+print(c+d)
+#concatenation
+print(e+f)
+
+#Exponential
+print(' 4 raise to power 3 is ',4**3)
+
+#booleanvalue
+print( f"is '6' with quote a int {type('6')==type(6)} .")
+
+#booleanvalue
+print( f"is '6' with quote a string {type('6')==type('9')} .")
