bb6b03e58ac59e7c7a8155d4a9b0b7664b718970
Surendra Bisht <surendra.bisht023@gmail.com>
2020-07-10 14:37:23 +0530

    Adding images folder and moving images to this folder

--- day7_functions/02_functionPractice.py
+++ day7_functions/02_functionPractice.py
@@ -1,11 +1,12 @@
-def GetCubeOfNo(no):
-    return f'Cube of {no} is {no**3}'
 
-# multiple function calls in one line where result of each function 
-# is passed as argument to another function.
-print( GetCubeOfNo( int( input("Enter the no: ") ) ) )
+# can be used to traverse each element in collection.
+# we have list of number which is collection, and we need to accees every element and do some operation 
+# on it used every no to created some data
+#
 
-
-
-
+# can be used to run any set ot statments multiple times defined.
+# for ex, here we are printing hello world 5 times as range produces total 
+#(0,1,2,3,4) elements and traversing these elements make it run statemments multiple times
+for no in range(5):
+   print("Hello world") 
  
