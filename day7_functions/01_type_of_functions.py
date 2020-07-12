b43097098cc9e569feeb43f69bdf09a0c54ea6c2
Surendra Bisht <surendra.bisht023@gmail.com>
2020-07-12 22:24:02 +0530

    image moved

--- day7_functions/01_type_of_functions.py
+++ day7_functions/01_type_of_functions.py
@@ -8,28 +8,7 @@ def GetSquareOfNo(no):
     return f'Square of  {no}  is {no*no}'
 
 def A_PlusB_Square(a,b):
+    print("asd")
     return a*a +b*b + 2*a*b
 
-def printHurrey():
-    print("Hurrey")
 
-
-printHurrey()
-x=int(input("Enter the value : "))
-y= GetSquareOfNo(x)
-print(y)
-
-printHurrey()
-
-z=A_PlusB_Square(20,5)
-print(z)
-
-printHurrey()
-
-x=GetDate()
-print(x)
-
-printHurrey()
-
-c=sum(23,56)
-print(c)
