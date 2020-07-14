3318fc07f8df0497d2c065561089ee6fac26ea1a
Surendra Bisht <surendra.bisht023@gmail.com>
2020-07-14 05:44:46 +0530

    added new folder

---
+++ Day7_functions/01_type_of_functions.py
@@ -0,0 +1,14 @@
+def sum(a,b):
+    return a+b
+
+def GetDate():
+    return '2020-07-06'
+
+def GetSquareOfNo(no):
+    return f'Square of  {no}  is {no*no}'
+
+def A_PlusB_Square(a,b):
+    print("asd")
+    return a*a +b*b + 2*a*b
+
+
