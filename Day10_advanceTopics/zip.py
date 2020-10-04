5c3cfec4bde95e77d53342ce7665258705f444c2
Anant Maheshwari <56751952+maheshwari2Anant@users.noreply.github.com>
2020-10-04 17:24:05 +0530

    Update zip.py

--- Day10_advanceTopics/zip.py
+++ Day10_advanceTopics/zip.py
@@ -1,6 +1,6 @@
-letter=['a','b','c','d']
+letter= str(input("enter the string =")) #enter alphabets
 no=[2,3,4,6]
-word= ["Alpha","beta","gamma",]
+word= ["Alpha","beta","gamma"]
 
 for item in zip(letter,no,word):
     print(item)
